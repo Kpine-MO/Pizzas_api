cfa1b2e8330297fb14e735537b574d8b8c5f1a80
Kpine-MO <chrispine.oduor@student.moringaschool.com>
2022-10-02 19:55:23 +0300

    commit of restaurants error message status not found

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,22 @@
 class RestaurantsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_restaurant_not_found_error
 
     def index
         restaurants = Restaurant.all
         render json: restaurants
+    end
+
+    def show
+        render json: find_restaurant
+    end
+
+    private
+
+    def find_restaurant
+        Restaurant.find(params[:id])
+    end
+
+    def render_restaurant_not_found_error
+        render json: {error: 'Restaurant not found'}, status: :not_found
     end
 end
