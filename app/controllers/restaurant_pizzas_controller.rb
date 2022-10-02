ac45659b8318351b4b8ca51274fe4120ae6c35ec
Kpine-MO <chrispine.oduor@student.moringaschool.com>
2022-10-02 20:53:19 +0300

    commit of invalid record error message on RestaurantPizza.create

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,7 +1,9 @@
 class RestaurantPizzasController < ApplicationController
 
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_error
+
     def create
-        restaurant_pizzas = RestaurantPizza.create(resPiz_params)
+        restaurant_pizzas = RestaurantPizza.create!(resPiz_params)
         pizza = restaurant_pizzas.pizza
         render json: pizza
     end
@@ -10,5 +12,9 @@ class RestaurantPizzasController < ApplicationController
 
     def resPiz_params
         params.permit(:price, :pizza_id, :restaurant_id)
+    end
+
+    def render_unprocessable_entity_error(invalid)
+       render json: {errors: invalid.record.errors}, status: :unprocessable_entity
     end
 end
