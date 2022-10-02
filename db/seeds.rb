fbcd8b4298318866078f80d584003a395bec3da5
Kpine-MO <chrispine.oduor@student.moringaschool.com>
2022-10-02 18:30:35 +0300

    commit of seeded data and model assosiation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+restaurant1 = Restaurant.create(name: "Galitos", address: "Kenyatta Avenue")
+
+restaurant2 = Restaurant.create(name: "KFC", address: "14th Avenue")
+
+restaurant3 = Restaurant.create(name: "McDonald", address: "Nyali Avenue")
+
+restaurant4 = Restaurant.create(name: "Subway", address: "Mombasa Road")
+
+pizza1 = Pizza.create(name: 'Neapolitan Pizza', ingredients: 'Marinara')
+
+pizza2 = Pizza.create(name: 'Chicago Pizza', ingredients: 'Pepperoni')
+
+pizza3 = Pizza.create(name: 'Silician Pizza', ingredients: 'Anchovies')
+
+pizza4 = Pizza.create(name: 'Greek Pizza', ingredients: 'Cheese')
+
+resPiz1 = RestaurantPizza.create(price: 10, pizza_id: pizza1.id, restaurant_id: restaurant4.id)
+
+resPiz2 = RestaurantPizza.create(price: 20, pizza_id: pizza3.id, restaurant_id: restaurant3.id)
+
+resPiz3 = RestaurantPizza.create(price: 10, pizza_id: pizza4.id, restaurant_id: restaurant2.id)
+
+resPiz4 = RestaurantPizza.create(price: 15, pizza_id: pizza2.id, restaurant_id: restaurant1.id)
