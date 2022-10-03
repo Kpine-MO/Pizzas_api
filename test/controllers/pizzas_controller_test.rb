be584308d0e61db8a27b30801d7e2241abfc9eda
Kpine-MO <chrispine.oduor@student.moringaschool.com>
2022-10-03 08:14:31 +0300

    commit tree 52303256818e43b9186f44aac9a01033d7f29b80

--- test/controllers/pizzas_controller_test.rb
+++ test/controllers/pizzas_controller_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class PizzasControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    assert(last_response.body).to include_json(
+      "id": 3,
+      "name": "Silician Pizza",
+      "ingredients": "Anchovies"
+    )
+  end
 end
